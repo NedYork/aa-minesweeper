fe38a82881e33779a55d81c4e172544ab7b204de
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-12-04 14:27:43 -0500

    Initialize Minesweeper class and implement render method.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,2 +1,28 @@
+require_relative "board.rb"
+require_relative "tile.rb"
+
 class Minesweeper
+
+  attr_reader :game
+  # When creating a new Minesweeper game, create a new board
+  def initialize
+    @game = Board.new
+  end
+
+  # iterate through the board and print out the corresponding symbol
+  def render
+    game.grid.each do |row|
+      row_output = []
+      row.each do |el|
+        if el.revealed
+          el_output = "_" #has to be changed
+        else
+          el_output = "*"
+        end
+        row_output << el_output
+      end
+      p row_output.join(" ")
+    end
+  end
+
 end
