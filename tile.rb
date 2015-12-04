bbb21290f05e02038dc2349faca9a55ace52fd10
App Academy Student <appacademy@Apps-Mac-mini-6.local>
2015-12-04 11:28:33 -0500

    Initialize Tile class and ivars.

--- tile.rb
+++ tile.rb
@@ -1,2 +1,14 @@
 class Tile
+
+  attr_reader :has_bomb
+  attr_accessor :has_flag, :revealed
+
+  def initialize
+    @has_bomb = false
+    @has_flag = false
+    @revealed = false
+  end
+
+  
+
 end
